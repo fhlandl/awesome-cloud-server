3176c07d5bb08b6e67e204191756c588320abc17
fhlandl <mkj1206@gmail.com>
2024-09-26 13:04:20 +0900

    init

---
+++ src/main/java/fhlandl/awesome_cloud_server/AwesomeCloudServerApplication.java
@@ -0,0 +1,13 @@
+package fhlandl.awesome_cloud_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeCloudServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhlandl/awesome_cloud_server/AwesomeCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fhlandl.awesome_cloud_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeCloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
